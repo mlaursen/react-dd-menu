83e3037e916336eaa48a70319bacaa27cdc2ccac
laursenm <laursenm@teds.com>
2015-10-09 08:10:40 -0400

    Upgraded to React 0.14.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Example from './Example'
 
-React.render(<Example />, document.body);
+ReactDOM.render(<Example />, document.getElementById('app'));
